9533a18702960ec7c75fb24dec6ef9ed3444acf7
webmobiledeveloper01 <126544719+webmobiledeveloper01@users.noreply.github.com>
2023-03-03 09:19:13 +0900

    Create consts.dart

---
+++ lib/utils/consts.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+const String LANGUAGE_CODE = 'languageCode';
+const String DEVICE_ADDRESS = 'device_address';
+const String DEVICE_NAME = 'device_name';
+
+List<Locale> languages = [
+  const Locale('en', 'US'),
+  const Locale('es', 'ES'),
+];
