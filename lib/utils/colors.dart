1a31c8e3029796d612866a302f999eb2ed5f4138
webmobiledeveloper01 <126544719+webmobiledeveloper01@users.noreply.github.com>
2023-03-03 09:19:10 +0900

    Create colors.dart

---
+++ lib/utils/colors.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+
+const Color background = Color(0xff303030);
+const Color foreground = Color(0xff595959);
+const Color orange = Color(0xffff9a00);
+const Color blue = Color(0xff125bb7);
+const Color appbar = orange;
